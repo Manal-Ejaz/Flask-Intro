6b6ce3e99ff79765cf19b52a13cf84ba93e52393
shehrozeee <33824420+shehrozeee@users.noreply.github.com>
2022-11-22 17:51:47 +0500

    fixed attribute name error

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ db=SQLAlchemy(app) #database initializing
 
 class Todo(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    content=db.Column(db.string(200),nullable=False)
+    content=db.Column(db.String(200),nullable=False)
     completed=db.Column(db.Integer,default=0)
     date_created=db.Column(db.DateTime,default=datetime.utcnow)
 
